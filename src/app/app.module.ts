419c52aced7260969e6d34dff107355652e4f2a5
lazarpe <lazar.petrovic.nefti@gmail.com>
2023-04-11 16:30:14 +0200

    implementation of the game.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { PlayingFieldComponent } from './components/playing-field/playing-field.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PlayingFieldComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/playing-field/playing-field.component.ts
@@ -0,0 +1,65 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-playing-field',
+  templateUrl: './playing-field.component.html',
+  styleUrls: ['./playing-field.component.css']
+})
+export class PlayingFieldComponent {
+  dictionary: string[] = ['banana', 'mango', 'apple', 'peach'];
+  word: string = this.getRandomWord();
+  letters: string[] = this.word.split('');
+  gameStarted: boolean = false;
+
+  alphabet: string[] = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  guessed_right_characters: string[] = [];
+  guessed_wrong_characters: string[] = [];
+
+  getRandomWord(): string {
+    return this.dictionary[Math.floor(Math.random() * this.dictionary.length)];
+  }
+
+  resetGame(): void {
+    this.gameStarted = true;
+    this.word = this.getRandomWord();
+    this.letters = this.word.split('');
+    this.guessed_right_characters = [];
+    this.guessed_wrong_characters = [];
+
+    for (let right_char of this.letters) {
+      this.guessed_right_characters.push('_');
+    }
+  }
+
+  guess(letter: string): void {
+    if (this.word.indexOf(letter) > -1) {
+      this.letters = this.letters.map((l) => {
+        if (l === letter) {
+          console.log('letter: ', letter)
+          this.letters.forEach((l, i) => {
+            if (l === letter) {
+              this.guessed_right_characters[i] = letter;
+            }
+          });
+
+          return letter;
+        } else {
+          console.log('l: ', l)
+          return l;
+        }
+      });
+      // check if array contains '_' anymore
+      if (this.guessed_right_characters.indexOf('_') === -1) {
+        this.gameStarted = false;
+        alert('You won!');
+      }
+    } else {
+      this.guessed_wrong_characters.push(letter);
+
+      if (this.guessed_wrong_characters.length === 10) {
+        this.gameStarted = false;
+        alert('Game over!');
+      }
+    }
+  }
+}
